375e491b977b88fab20829cf49ba4445cb98294a
balikotamas <balikotamas95@gmail.com>
2019-07-17 15:34:19 +0200

    add Apple

---
+++ week-4/day 03/Apple/MyApp/MyApp_lib/apples.cpp
@@ -0,0 +1,10 @@
+//
+// Created by balas on 2019. 07. 17..
+//
+
+#include "apples.h"
+
+std::string getApple()
+{
+    return "apple";
+}
---
+++ week-4/day 03/Apple/MyApp_lib/apples.h
@@ -0,0 +1,11 @@
+//
+// Created by balas on 2019. 07. 17..
+//
+
+#ifndef MYAPP_APPLES_H
+#define MYAPP_APPLES_H
+
+#include <string>
+std::string getApple();
+
+#endif //MYAPP_APPLES_H
